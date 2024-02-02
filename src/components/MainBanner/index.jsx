0ea949c5c02ee971e2740d13a630b732484af099
Matheus Pano <panomatheus@gmail.com>
2024-02-02 17:06:12 -0300

    .

--- src/components/MainBanner/index.jsx
+++ src/components/MainBanner/index.jsx
@@ -1,5 +1,8 @@
-import Slider from "react-slick";
-import useWindowWidth from '../WindowWidth/index';
+//import Slider from "react-slick";
+import useWindowWidth from "../WindowWidth/index";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 import banner from "../../assets/main-banner.png";
 import bannerMobile from "../../assets/main-banner-mobile.png";
@@ -7,21 +10,44 @@ import bannerMobile from "../../assets/main-banner-mobile.png";
 import "./style.scss";
 
 const MainBanner = () => {
-  const { width } = useWindowWidth();
+    const { width } = useWindowWidth();
 
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
+    /*var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+    };*/
     
-  };
 
-  return (
-    <div className="home-mainBanner">
-      <Slider {...settings}>
+
+    return (
+        <div className="home-mainBanner">
+            <Carousel autoPlay={false} showArrows={true} showThumbs={false} >
+                <div>
+                    <img
+                        src={width < 1024 ? bannerMobile : banner}
+                        alt="Banner"
+                    />
+                </div>
+                <div>
+                    <img
+                        src={width < 1024 ? bannerMobile : banner}
+                        alt="Banner"
+                    />
+                </div>
+                <div>
+                    <img
+                        src={width < 1024 ? bannerMobile : banner}
+                        alt="Banner"
+                    />
+                </div>
+            </Carousel>
+            
+
+            {/*<Slider {...settings}>
         {
           [...Array(5)].map((_, i) => {
             return (
@@ -31,9 +57,9 @@ const MainBanner = () => {
             )
           })
         }
-      </Slider>
-    </div>
-  );
+      </Slider>*/}
+        </div>
+    );
 };
 
 export default MainBanner;
